4d7814fe1b89632d5760d573af402591f66f5182
Alex <garbieto@fi.uba.ar>
2021-05-23 20:58:15 -0500

    [2] done

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,23 @@ mod commands;
 mod database;
 mod server;
 mod storagevalue;
+mod redis;
+mod config_parser;
 
-use server::Server;
+use std::env;
+use redis::Redis;
+
+fn get_path() -> Result<String, String> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        return Err("Invalid number of arguments".to_string());
+    }
+    
+    Ok(args.get(1).unwrap().to_string())
+}
 
 fn main() {
-    let server = Server::new("0.0.0.0:8888");
-    server.run();
+    let config_path = get_path().unwrap();
+    let redis = Redis::new(&config_path).unwrap();
+    redis.run();
 }
