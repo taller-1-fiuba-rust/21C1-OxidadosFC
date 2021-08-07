dbd540cf1d7751e523b365efa97dd41446cf3f91
Francisco Strambini <franfiuba@gmail.com>
2021-08-07 19:30:20 -0300

    list of responses

--- src/http_server/http_server/src/main.rs
+++ src/http_server/http_server/src/main.rs
@@ -20,23 +20,37 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let mut redis_stream = TcpStream::connect("127.0.0.1:8888").unwrap();
-
+    let record: Vec<(String, String)> = Vec::new();
     loop {
         let mut buffer = [0; 1024];
-
         let bytes_read = stream.read(&mut buffer).unwrap();
         let request = std::str::from_utf8(&buffer[..bytes_read]).unwrap();
-        let post = b"POST / HTTP/1.1\r\n";
-        let (status_line, filename) = if buffer.starts_with(post) {
+
+        let (status_line, contents) = if buffer.starts_with(b"POST / HTTP/1.1\r\n") {
             let command = get_command(request);
             let response = handle_redis_connection(&redis_stream, command);
-        
-            ("HTTP/1.1 200 OK\r\n\r\n", "src/home.html")
+            record.push((command, response));
+
+            let contents: &str = fs::read_to_string("src/index.html").unwrap();
+
+            println!("{}", contents);
+
+            let body_pos = contents
+                .iter()
+                .position(|x| x.contains(r#"<div id="answer">"#))
+                .unwrap();
+
+            let contents = contents.insert(body_pos, 'h');
+
+            ("HTTP/1.1 201 OK\r\n\r\n", contents)
         } else {
-            ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "src/home.html")
+            let contents = fs::read_to_string("src/index.html").unwrap();
+
+            ("HTTP/1.1 200 OK\r\n\r\n", contents)
         };
-        let contents = fs::read_to_string(filename).unwrap();
+
         let response = format!("{}{}", status_line, contents);
+
         stream.write(response.as_bytes()).unwrap();
         stream.flush().unwrap();
     }
@@ -50,7 +64,7 @@ fn get_command(request: &str) -> String {
     command
 }
 
-fn handle_redis_connection(mut stream: &TcpStream, command: String) -> String{
+fn handle_redis_connection(mut stream: &TcpStream, command: String) -> String {
     stream.write(command.as_bytes()).unwrap();
     let mut buffer_respond = [0; 1024];
     let bytes_read = stream.read(&mut buffer_respond).unwrap();
