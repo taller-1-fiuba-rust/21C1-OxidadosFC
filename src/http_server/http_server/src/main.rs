fc80df9e768a5aa79519c0c6267e2e2398c7a1de
Francisco Strambini <franfiuba@gmail.com>
2021-08-07 20:13:40 -0300

    list of request reponses

--- src/http_server/http_server/src/main.rs
+++ src/http_server/http_server/src/main.rs
@@ -20,7 +20,7 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let mut redis_stream = TcpStream::connect("127.0.0.1:8888").unwrap();
-    let record: Vec<(String, String)> = Vec::new();
+    let mut record: Vec<(String, String)> = Vec::new();
     loop {
         let mut buffer = [0; 1024];
         let bytes_read = stream.read(&mut buffer).unwrap();
@@ -28,19 +28,19 @@ fn handle_connection(mut stream: TcpStream) {
 
         let (status_line, contents) = if buffer.starts_with(b"POST / HTTP/1.1\r\n") {
             let command = get_command(request);
-            let response = handle_redis_connection(&redis_stream, command);
-            record.push((command, response));
+            let response = handle_redis_connection(&redis_stream, &command);
+            
+            record.push((command.to_string(), response.to_string()));
 
-            let contents: &str = fs::read_to_string("src/index.html").unwrap();
+            let contents = fs::read_to_string("src/index.html").unwrap();
 
-            println!("{}", contents);
+            let list = build_list(&record);
 
-            let body_pos = contents
-                .iter()
-                .position(|x| x.contains(r#"<div id="answer">"#))
-                .unwrap();
-
-            let contents = contents.insert(body_pos, 'h');
+            let contents =  contents.replace(r#"<div id="answer"></div>"#, &list.join(""));
+            // let body_pos = contents
+            //     .iter()
+            //     .position(|x| x.contains(r#"<div id="answer">"#))
+            //     .unwrap();
 
             ("HTTP/1.1 201 OK\r\n\r\n", contents)
         } else {
@@ -64,10 +64,22 @@ fn get_command(request: &str) -> String {
     command
 }
 
-fn handle_redis_connection(mut stream: &TcpStream, command: String) -> String {
+fn handle_redis_connection(mut stream: &TcpStream, command: &String) -> String {
     stream.write(command.as_bytes()).unwrap();
     let mut buffer_respond = [0; 1024];
     let bytes_read = stream.read(&mut buffer_respond).unwrap();
     let response = std::str::from_utf8(&buffer_respond[..bytes_read]).unwrap();
     response.to_string()
+}
+
+fn build_list(records: &Vec<(String, String)>) -> Vec<String> {
+    let  mut list_elements: Vec<String> = records.iter().map(|(req, res)| {
+        format!("<li>Request: {} Reponse: {}</li>", req, res)
+    }).collect();
+
+    list_elements.insert(0, "<ol>".to_string());
+    list_elements.push("</ol>".to_string());
+
+
+    list_elements
 }
