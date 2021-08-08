52810592969dc337b374467b523cce531241dcc5
Alex <garbieto@fi.uba.ar>
2021-08-08 14:20:42 -0500

    multiclient in a browser error fixed

--- src/http_server/http_server/src/main.rs
+++ src/http_server/http_server/src/main.rs
@@ -4,8 +4,10 @@ use std::net::TcpListener;
 use std::net::TcpStream;
 use std::sync::{Arc, Mutex};
 use std::thread;
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    let redis_stream = TcpStream::connect("127.0.0.1:8888").unwrap();
 
     let records: Arc<Mutex<Vec<(String, String)>>> = Arc::new(Mutex::new(Vec::new()));
 
@@ -14,17 +16,20 @@ fn main() {
             Err(e) => eprintln!("failed: {}", e),
             Ok(stream) => {
                 let new_records = records.clone();
+                let redis_stream = redis_stream.try_clone().unwrap();
                 thread::spawn(move || {
-                    handle_connection(stream, new_records);
+                    handle_connection(stream, new_records, redis_stream);
                 });
             }
         }
     }
 }
 
-fn handle_connection(mut stream: TcpStream,records: Arc<Mutex<Vec<(String, String)>>>) {
-    let mut redis_stream = TcpStream::connect("127.0.0.1:8888").unwrap();
-
+fn handle_connection(
+    mut stream: TcpStream,
+    records: Arc<Mutex<Vec<(String, String)>>>,
+    redis_stream: TcpStream,
+) {
     let mut buffer = [0; 2048];
 
     let bytes_read = stream.read(&mut buffer).unwrap();
@@ -35,9 +40,9 @@ fn handle_connection(mut stream: TcpStream,records: Arc<Mutex<Vec<(String, Strin
         let command = get_command(request);
         let response = handle_redis_connection(&redis_stream, &command);
 
-        let mut records_guard = records.lock().unwrap(); 
+        let mut records_guard = records.lock().unwrap();
 
-        records_guard.push((command.to_string(), response.to_string()));
+        records_guard.push((command, response));
 
         let contents = fs::read_to_string("src/index.html").unwrap();
         let answer = build_answer(&records_guard);
@@ -52,9 +57,7 @@ fn handle_connection(mut stream: TcpStream,records: Arc<Mutex<Vec<(String, Strin
 
     let response = format!("{}{}", status_line, contents);
 
-    println!("{}", response);
-
-    stream.write(response.as_bytes()).unwrap();
+    stream.write_all(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
 
@@ -83,7 +86,7 @@ fn secure_read(mut stream: &TcpStream) -> String {
     response
 }
 
-fn build_answer(records: &Vec<(String, String)>) -> String {
+fn build_answer(records: &[(String, String)]) -> String {
     let mut list_elements: Vec<String> = records
         .iter()
         .map(|(req, res)| format!("<li>Request: {} Reponse: {}</li>", req, res))
