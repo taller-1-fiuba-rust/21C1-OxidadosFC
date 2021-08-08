428675b2ebe0ee75c4d65d9d4f02155052a3b289
Francisco Strambini <franfiuba@gmail.com>
2021-08-08 14:02:57 -0300

    fix http statles

--- src/http_server/http_server/src/main.rs
+++ src/http_server/http_server/src/main.rs
@@ -2,58 +2,60 @@ use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
+use std::sync::{Arc, Mutex};
 use std::thread;
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+
+    let records: Arc<Mutex<Vec<(String, String)>>> = Arc::new(Mutex::new(Vec::new()));
 
     for stream in listener.incoming() {
         match stream {
             Err(e) => eprintln!("failed: {}", e),
             Ok(stream) => {
+                let new_records = records.clone();
                 thread::spawn(move || {
-                    handle_connection(stream);
+                    handle_connection(stream, new_records);
                 });
             }
         }
     }
 }
 
-fn handle_connection(mut stream: TcpStream) {
+fn handle_connection(mut stream: TcpStream,records: Arc<Mutex<Vec<(String, String)>>>) {
     let mut redis_stream = TcpStream::connect("127.0.0.1:8888").unwrap();
-    let mut record: Vec<(String, String)> = Vec::new();
-    loop {
-        let mut buffer = [0; 1024];
-        let bytes_read = stream.read(&mut buffer).unwrap();
-        let request = std::str::from_utf8(&buffer[..bytes_read]).unwrap();
 
-        let (status_line, contents) = if buffer.starts_with(b"POST / HTTP/1.1\r\n") {
-            let command = get_command(request);
-            let response = handle_redis_connection(&redis_stream, &command);
-            
-            record.push((command.to_string(), response.to_string()));
+    let mut buffer = [0; 2048];
 
-            let contents = fs::read_to_string("src/index.html").unwrap();
+    let bytes_read = stream.read(&mut buffer).unwrap();
 
-            let list = build_list(&record);
+    let request = std::str::from_utf8(&buffer[..bytes_read]).unwrap();
 
-            let contents =  contents.replace(r#"<div id="answer"></div>"#, &list.join(""));
-            // let body_pos = contents
-            //     .iter()
-            //     .position(|x| x.contains(r#"<div id="answer">"#))
-            //     .unwrap();
+    let (status_line, contents) = if buffer.starts_with(b"POST / HTTP/1.1\r\n") {
+        let command = get_command(request);
+        let response = handle_redis_connection(&redis_stream, &command);
 
-            ("HTTP/1.1 201 OK\r\n\r\n", contents)
-        } else {
-            let contents = fs::read_to_string("src/index.html").unwrap();
+        let mut records_guard = records.lock().unwrap(); 
 
-            ("HTTP/1.1 200 OK\r\n\r\n", contents)
-        };
+        records_guard.push((command.to_string(), response.to_string()));
 
-        let response = format!("{}{}", status_line, contents);
+        let contents = fs::read_to_string("src/index.html").unwrap();
+        let answer = build_answer(&records_guard);
+        let contents = contents.replace(r#"<div id="answer"></div>"#, &answer);
 
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
-    }
+        ("HTTP/1.1 201 OK\r\n\r\n", contents)
+    } else {
+        let contents = fs::read_to_string("src/index.html").unwrap();
+
+        ("HTTP/1.1 200 OK\r\n\r\n", contents)
+    };
+
+    let response = format!("{}{}", status_line, contents);
+
+    println!("{}", response);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
 
 fn get_command(request: &str) -> String {
@@ -72,14 +74,17 @@ fn handle_redis_connection(mut stream: &TcpStream, command: &String) -> String {
     response.to_string()
 }
 
-fn build_list(records: &Vec<(String, String)>) -> Vec<String> {
-    let  mut list_elements: Vec<String> = records.iter().map(|(req, res)| {
-        format!("<li>Request: {} Reponse: {}</li>", req, res)
-    }).collect();
+fn build_answer(records: &Vec<(String, String)>) -> String {
+    let mut list_elements: Vec<String> = records
+        .iter()
+        .map(|(req, res)| format!("<li>Request: {} Reponse: {}</li>", req, res))
+        .collect();
 
     list_elements.insert(0, "<ol>".to_string());
     list_elements.push("</ol>".to_string());
 
+    list_elements.insert(0, r#"<div id="answer">"#.to_string());
+    list_elements.insert(0, "</div>".to_string());
 
-    list_elements
+    list_elements.join("")
 }
